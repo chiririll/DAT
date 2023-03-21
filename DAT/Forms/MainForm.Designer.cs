3fa397174c6d894a46737ccfa840c559b4f6e322
Kirill Chizhov <sstive39@gmail.com>
2023-03-22 00:09:26 +0300

    вариант 1

--- DAT/Forms/MainForm.Designer.cs
+++ DAT/Forms/MainForm.Designer.cs
@@ -38,7 +38,15 @@
             this.viewHelpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
             this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sidebar = new System.Windows.Forms.FlowLayoutPanel();
+            this.memTypeLabel = new System.Windows.Forms.Label();
+            this.memType = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.memSizeLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.menuStrip.SuspendLayout();
+            this.sidebar.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip
@@ -107,19 +115,97 @@
             // 
             this.viewHelpToolStripMenuItem.Name = "viewHelpToolStripMenuItem";
             this.viewHelpToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F1;
-            this.viewHelpToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.viewHelpToolStripMenuItem.Size = new System.Drawing.Size(224, 34);
             this.viewHelpToolStripMenuItem.Text = "&View Help";
             // 
             // toolStripSeparator2
             // 
             this.toolStripSeparator2.Name = "toolStripSeparator2";
-            this.toolStripSeparator2.Size = new System.Drawing.Size(267, 6);
+            this.toolStripSeparator2.Size = new System.Drawing.Size(221, 6);
             // 
             // aboutToolStripMenuItem
             // 
             this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
-            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(224, 34);
             this.aboutToolStripMenuItem.Text = "&About";
+            // 
+            // sidebar
+            // 
+            this.sidebar.Controls.Add(this.memTypeLabel);
+            this.sidebar.Controls.Add(this.memType);
+            this.sidebar.Controls.Add(this.memSizeLabel);
+            this.sidebar.Controls.Add(this.textBox1);
+            this.sidebar.Controls.Add(this.label1);
+            this.sidebar.Controls.Add(this.panel1);
+            this.sidebar.Dock = System.Windows.Forms.DockStyle.Left;
+            this.sidebar.Location = new System.Drawing.Point(0, 33);
+            this.sidebar.MinimumSize = new System.Drawing.Size(200, 300);
+            this.sidebar.Name = "sidebar";
+            this.sidebar.Size = new System.Drawing.Size(300, 417);
+            this.sidebar.TabIndex = 1;
+            // 
+            // memTypeLabel
+            // 
+            this.memTypeLabel.AutoSize = true;
+            this.memTypeLabel.Location = new System.Drawing.Point(3, 10);
+            this.memTypeLabel.Margin = new System.Windows.Forms.Padding(3, 10, 3, 0);
+            this.memTypeLabel.Name = "memTypeLabel";
+            this.memTypeLabel.Size = new System.Drawing.Size(168, 20);
+            this.memTypeLabel.TabIndex = 0;
+            this.memTypeLabel.Text = "Организация памяти";
+            // 
+            // memType
+            // 
+            this.memType.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.memType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.memType.FormattingEnabled = true;
+            this.memType.Items.AddRange(new object[] {
+            "Страничная",
+            "Сегментная",
+            "Странично-сегментная"});
+            this.memType.Location = new System.Drawing.Point(5, 33);
+            this.memType.Margin = new System.Windows.Forms.Padding(5, 3, 3, 3);
+            this.memType.MaximumSize = new System.Drawing.Size(300, 0);
+            this.memType.MinimumSize = new System.Drawing.Size(150, 0);
+            this.memType.Name = "memType";
+            this.memType.Size = new System.Drawing.Size(282, 28);
+            this.memType.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 146);
+            this.label1.Margin = new System.Windows.Forms.Padding(3, 20, 3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(210, 26);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Параметры памяти";
+            // 
+            // memSizeLabel
+            // 
+            this.memSizeLabel.AutoSize = true;
+            this.memSizeLabel.Location = new System.Drawing.Point(3, 74);
+            this.memSizeLabel.Margin = new System.Windows.Forms.Padding(3, 10, 3, 0);
+            this.memSizeLabel.Name = "memSizeLabel";
+            this.memSizeLabel.Size = new System.Drawing.Size(121, 20);
+            this.memSizeLabel.TabIndex = 3;
+            this.memSizeLabel.Text = "Объем памяти";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 97);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(284, 26);
+            this.textBox1.TabIndex = 4;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(3, 175);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(284, 146);
+            this.panel1.TabIndex = 5;
             // 
             // MainForm
             // 
@@ -127,13 +213,15 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.sidebar);
             this.Controls.Add(this.menuStrip);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MainMenuStrip = this.menuStrip;
             this.Name = "MainForm";
             this.Text = "Dynamic Address Translation Model";
             this.menuStrip.ResumeLayout(false);
             this.menuStrip.PerformLayout();
+            this.sidebar.ResumeLayout(false);
+            this.sidebar.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -151,5 +239,12 @@
         private System.Windows.Forms.ToolStripMenuItem viewHelpToolStripMenuItem;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
         private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.FlowLayoutPanel sidebar;
+        private System.Windows.Forms.Label memTypeLabel;
+        private System.Windows.Forms.ComboBox memType;
+        private System.Windows.Forms.Label memSizeLabel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
